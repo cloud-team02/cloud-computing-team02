02885cda2ab4a4ef86b0028a868557f82a367217
Shuo Zhou <sz144@outlook.com>
2017-04-26 15:09:39 +0100

    Create Account.java

---
+++ src/cloud/clouddb/cloud_account/Account.java
@@ -0,0 +1,27 @@
+package cloud.clouddb.cloud_account;
+
+public class Account{
+	private int id;
+	private int user_id;
+	private double balance;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
