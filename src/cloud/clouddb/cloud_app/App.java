12fd376404737c5bb84d13b405ba5a01d763a48d
Yongshuo <nkniyongshuo@163.com>
2017-04-26 15:45:44 +0100

    Update App.java

--- src/cloud/clouddb/cloud_app/App.java
+++ src/cloud/clouddb/cloud_app/App.java
@@ -4,6 +4,8 @@ public class App{
 	private String App_name;
 	private int App_id;
 	private int Developer_id;
+	private int use_times;
+	private double price;
 	private String Url;
 	
 	public String getApp_name() {
@@ -24,12 +26,25 @@ public class App{
 	public void setDeveloper_id(int developer_id) {
 		this.Developer_id = developer_id;
 	}
+	public int getUse_times() {
+		return use_times;
+	}
+	public void setUse_times(int use_times) {
+		this.use_times = use_times;
+	}
 	public String getUrl() {
 		return Url;
 	}
 	public void setUrl(String url) {
 		this.Url = url;
 	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	
 	@Override
 	public String toString()
