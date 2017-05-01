be81ffae9dbcbcaf2f090be1f3374d124d6f361c
Shuo Zhou <sz144@outlook.com>
2017-05-01 20:56:00 +0100

    Add files via upload

--- src/cloud/clouddb/cloud_app/App.java
+++ src/cloud/clouddb/cloud_app/App.java
@@ -4,10 +4,29 @@ public class App{
 	private String App_name;
 	private int App_id;
 	private int Developer_id;
-	private String url;
-	private double price;
+	private String Url;
 	private int use_times;
+	private double price;
+	private boolean running = false;
 	
+	public int getUse_times() {
+		return use_times;
+	}
+	public void setUse_times(int use_times) {
+		this.use_times = use_times;
+	}
+	public boolean isRunning() {
+		return running;
+	}
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	public String getApp_name() {
 		return App_name;
 	}
@@ -27,27 +46,24 @@ public class App{
 		this.Developer_id = developer_id;
 	}
 	public String getUrl() {
-		return url;
+		return Url;
 	}
 	public void setUrl(String url) {
-		this.url = url;
+		this.Url = url;
 	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	public int getUse_times() {
-		return use_times;
-	}
-	public void setUse_times(int use_times) {
-		this.use_times = use_times;
-	}
+	
 	@Override
 	public String toString()
 	{
 		String str=this.App_id +" - "+ this.App_name;
 		return str;
+	}
+	
+	public void start(){
+		
+	}
+	
+	public void stop(){
+		
 	}
 }
