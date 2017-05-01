5834d41f6aaf3b4adde8021fee5842008105531c
Shuo Zhou <sz144@outlook.com>
2017-05-01 20:41:56 +0100

    Add files via upload

--- src/cloud/servlet/UploadServlet.java
+++ src/cloud/servlet/UploadServlet.java
@@ -7,12 +7,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 import javax.servlet.annotation.MultipartConfig;
 
 import java.util.Enumeration;
+//import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import cloud.clouddb.cloud_app.*;
+import cloud.clouddb.cloud_user.*;
 
 /**
  * Servlet implementation class UploadServlet
@@ -44,17 +48,42 @@ public class UploadServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String appPath = request.getServletContext().getRealPath("");
+		AppDAO appDAO = new AppDAO();
+	    App newApp = new App();
+	    newApp.setApp_name(request.getParameter("AppsName"));
+	    newApp.setPrice(Integer.parseInt(request.getParameter("price")));
+	    newApp.setDescription(request.getParameter("description"));
+	    newApp.setUse_times(0);
+	    HttpSession session = request.getSession();
+	    User developer = (User)session.getAttribute("currentSessionUser");
+	    //newApp.setDeveloper_id(developer.getId());
+		//String appPath = request.getServletContext().getRealPath("");
 	    // Create path to the directory to save uploaded file
-		String savePath = "/Users/NINI/Desktop/"+SAVE_DIR; //appPath + File.separator + SAVE_DIR;    
+		String savePath = "/home/sz144/tomcat/webapps/"+request.getParameter("AppsName"); //appPath + File.separator + SAVE_DIR;    
 	    // Create the save directory if it does not exist
 		File fileSaveDir = new File(savePath);
 		if (!fileSaveDir.exists())
 			fileSaveDir.mkdir();
-		for (Part part : request.getParts()) {
+		newApp.setUrl(savePath);
+		Part appPart = request.getPart("app_file");
+		Part iconPart = request.getPart("icon_file");
+		String fileName = extractFileName(appPart);
+		String iconName = extractFileName(iconPart);
+		String appPath = savePath + File.separator + fileName;
+		String iconPath = savePath + File.separator + iconName;
+		newApp.setIconUrl(iconPath);
+		appDAO.addApp(newApp);
+		appPart.write(appPath);
+		jarExtractor(appPath,savePath);
+		appPart.write(iconPath);
+		/*
+		for (Part part : request.getParts()){
 			String fileName = extractFileName(part);
+			System.out.println(fileName);
 			part.write(savePath + File.separator + fileName);
 		}
+		*/
+		
 		//response.getWriter().append(savePath);
 
 	}
@@ -68,5 +97,40 @@ public class UploadServlet extends HttpServlet {
 	    }
 	    return "";
 	  }
+	
+
+		//private statiString jarpath = "/home/sz144/tomcat/webapps/Due.jar";
+		//private static String destdir = "/home/sz144/tomcat/webapps/Due/";
+
+
+	  	// Main method extracts a jarfile to a set of
+	  	// directories under the target/path/ directory
+
+	  	public void jarExtractor(String jarpath, String destdir) throws IOException {
+	  		JarFile jarfile = new JarFile(jarpath);   
+		  	for (Enumeration<JarEntry> iter = jarfile.entries(); 
+		  			iter.hasMoreElements();) {   
+		  		JarEntry entry = iter.nextElement();   
+		  	    System.out.println("Processing: " + entry.getName());  
+		  	    File outfile = new File(destdir, entry.getName());
+		  	    if (! outfile.exists())
+		  	    	outfile.getParentFile().mkdirs(); // create dir structure
+
+		  	    // method body continued on next slide...
+		  	    // continuation of main method
+
+		  	    if (! entry.isDirectory()) {
+		  	      InputStream instream = jarfile.getInputStream(entry);
+		  	      FileOutputStream outstream = new FileOutputStream(outfile);
+		  	      while (instream.available() > 0) { 
+		  	          outstream.write(instream.read());
+		  	      }
+		  	      outstream.close();
+		  	      instream.close();
+		  	    }  // end if
+		  	  }  // end for
+		  	  jarfile.close();
+		  	}  // end main // end class
+
 
 }
